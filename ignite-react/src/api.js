6edccb9053849e517dd7d70f9c85af9a1310c987
axdamx <axdamx@gmail.com>
2020-11-15 22:16:07 +0800

    link redux with the apps, calling api

--- ignite-react/src/api.js
+++ ignite-react/src/api.js
@@ -1,8 +1,7 @@
 //Base URL
-
 const base_url = "https://api.rawg.io/api/";
 
-//Get the date
+//Getting the date
 const getCurrentMonth = () => {
   const month = new Date().getMonth() + 1;
   if (month < 10) {
@@ -11,7 +10,7 @@ const getCurrentMonth = () => {
     return month;
   }
 };
-
+//Getting the date
 const getCurrentDay = () => {
   const day = new Date().getDate();
   if (day < 10) {
@@ -21,16 +20,19 @@ const getCurrentDay = () => {
   }
 };
 
-// current day/month/year
-const currentYear = new Date().getFullYear;
+//Current day/month/year
+const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
-//Popular games
-
+//Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
-export const popularGamesUrl = () => `${base_url}${popular_games}`;
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
